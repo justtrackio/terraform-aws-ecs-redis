6b0f0b7576d9ba516a01d1edcc926eb964ad326e
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-04-27 10:49:15 +0200

    feat: create_module

--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
-variable "container_name" {
-  type        = string
-  description = "The name of the container. Up to 255 characters ([a-z], [A-Z], [0-9], -, _ allowed)"
+variable "container_cpu" {
+  type        = number
+  description = "The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value"
+  default     = 25
 }
 
 variable "container_image_repository" {
   type        = string
   description = "The image repository used to start the container. Images in the Docker Hub registry available by default"
+  default     = "redis"
 }
 
 variable "container_image_tag" {
   type        = string
   description = "The image tag used to start the container. Images in the Docker Hub registry available by default"
+  default     = "7-alpine"
 }
 
 variable "container_memory_reservation" {
@@ -19,10 +22,10 @@ variable "container_memory_reservation" {
   default     = 50
 }
 
-variable "container_cpu" {
-  type        = number
-  description = "The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value"
-  default     = 25
+variable "container_name" {
+  type        = string
+  description = "The name of the container. Up to 255 characters ([a-z], [A-Z], [0-9], -, _ allowed)"
+  default     = "redis"
 }
 
 variable "deployment_maximum_percent" {
@@ -35,11 +38,6 @@ variable "deployment_minimum_healthy_percent" {
   type        = number
   description = "The lower limit (as a percentage of `desired_count`) of the number of tasks that must remain running and healthy in a service during a deployment"
   default     = 0
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "The VPC ID where resources are created"
 }
 
 variable "ecs_cluster_arn" {
@@ -59,13 +57,10 @@ variable "network_mode" {
   default     = null
 }
 
-variable "service_placement_constraints" {
-  type = list(object({
-    type       = string
-    expression = string
-  }))
-  description = "The rules that are taken into consideration during task placement. Maximum number of placement_constraints is 10. See [`placement_constraints`](https://www.terraform.io/docs/providers/aws/r/ecs_service.html#placement_constraints-1) docs"
-  default     = []
+variable "redis_maxmemory" {
+  type        = number
+  description = "Maxmemory is a Redis configuration that allows you to set the memory limit at which your eviction policy takes effect."
+  default     = 25
 }
 
 variable "service_discovery_name" {
@@ -78,8 +73,16 @@ variable "service_discovery_namespace_id" {
   description = "The ID of the namespace that you want to use to create the service."
 }
 
-variable "redis_maxmemory" {
-  type        = number
-  description = "Maxmemory is a Redis configuration that allows you to set the memory limit at which your eviction policy takes effect."
-  default     = 25
+variable "service_placement_constraints" {
+  type = list(object({
+    type       = string
+    expression = string
+  }))
+  description = "The rules that are taken into consideration during task placement. Maximum number of placement_constraints is 10. See [`placement_constraints`](https://www.terraform.io/docs/providers/aws/r/ecs_service.html#placement_constraints-1) docs"
+  default     = []
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The VPC ID where resources are created"
 }
