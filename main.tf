3dc764c1adbfac4b54b6a362f0cfde4d5a9e2aa5
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-04-27 10:28:59 +0200

    feat: create_module

---
+++ main.tf
@@ -0,0 +1,75 @@
+module "container_definition" {
+  count   = module.this.enabled ? 1 : 0
+  source  = "cloudposse/ecs-container-definition/aws"
+  version = "0.58.3"
+
+  container_image              = "${var.container_image_repository}:${var.container_image_tag}"
+  container_name               = var.container_name
+  container_cpu                = var.container_cpu
+  container_memory_reservation = var.container_memory_reservation
+
+  port_mappings = [
+    {
+      containerPort = 6379
+      hostPort      = 0
+      protocol      = "tcp"
+    },
+  ]
+
+  command = [
+    "--maxmemory ${var.redis_maxmemory}mb",
+    "--maxmemory-policy allkeys-lru"
+  ]
+}
+
+module "redis" {
+  count   = module.this.enabled ? 1 : 0
+  source  = "cloudposse/ecs-alb-service-task/aws"
+  version = "0.68.0"
+
+  context = module.this.context
+
+  container_definition_json          = "[${sensitive(module.container_definition[0].json_map_encoded)}]"
+  deployment_maximum_percent         = var.deployment_maximum_percent
+  deployment_minimum_healthy_percent = var.deployment_minimum_healthy_percent
+  ecs_cluster_arn                    = var.ecs_cluster_arn
+  launch_type                        = var.launch_type
+  network_mode                       = var.network_mode
+  vpc_id                             = var.vpc_id
+
+  service_registries = [{
+    registry_arn   = aws_service_discovery_service.default[0].arn
+    container_name = var.container_name
+    container_port = 6379
+  }]
+
+  tags = {
+    Model           = "kvstore_${try(module.this.attributes[0], "")}"
+    ApplicationType = "redis"
+  }
+
+  service_placement_constraints = var.service_placement_constraints != null ? var.service_placement_constraints : module.this.environment == "prod" ? [{
+    type       = "memberOf"
+    expression = "attribute:spotinst.io/container-instance-lifecycle==od"
+  }] : []
+}
+
+resource "aws_service_discovery_service" "default" {
+  count = module.this.enabled ? 1 : 0
+  name  = var.service_discovery_name
+
+  dns_config {
+    namespace_id = var.service_discovery_namespace_id
+
+    dns_records {
+      ttl  = 60
+      type = "SRV"
+    }
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+
+  tags = module.this.tags
+}
---
+++ variables.tf
@@ -0,0 +1,85 @@
+variable "container_name" {
+  type        = string
+  description = "The name of the container. Up to 255 characters ([a-z], [A-Z], [0-9], -, _ allowed)"
+}
+
+variable "container_image_repository" {
+  type        = string
+  description = "The image repository used to start the container. Images in the Docker Hub registry available by default"
+}
+
+variable "container_image_tag" {
+  type        = string
+  description = "The image tag used to start the container. Images in the Docker Hub registry available by default"
+}
+
+variable "container_memory_reservation" {
+  type        = number
+  description = "The amount of memory (in MiB) to reserve for the container. If container needs to exceed this threshold, it can do so up to the set container_memory hard limit"
+  default     = 50
+}
+
+variable "container_cpu" {
+  type        = number
+  description = "The number of cpu units to reserve for the container. This is optional for tasks using Fargate launch type and the total amount of container_cpu of all containers in a task will need to be lower than the task-level cpu value"
+  default     = 25
+}
+
+variable "deployment_maximum_percent" {
+  type        = number
+  description = "The upper limit of the number of tasks (as a percentage of `desired_count`) that can be running in a service during a deployment"
+  default     = 100
+}
+
+variable "deployment_minimum_healthy_percent" {
+  type        = number
+  description = "The lower limit (as a percentage of `desired_count`) of the number of tasks that must remain running and healthy in a service during a deployment"
+  default     = 0
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "The VPC ID where resources are created"
+}
+
+variable "ecs_cluster_arn" {
+  type        = string
+  description = "The ARN of the ECS cluster where service will be provisioned"
+}
+
+variable "launch_type" {
+  type        = string
+  description = "The launch type on which to run your service. Valid values are `EC2` and `FARGATE`"
+  default     = "EC2"
+}
+
+variable "network_mode" {
+  type        = string
+  description = "The network mode to use for the task. This is required to be `awsvpc` for `FARGATE` `launch_type` or `null` for `EC2` `launch_type`"
+  default     = null
+}
+
+variable "service_placement_constraints" {
+  type = list(object({
+    type       = string
+    expression = string
+  }))
+  description = "The rules that are taken into consideration during task placement. Maximum number of placement_constraints is 10. See [`placement_constraints`](https://www.terraform.io/docs/providers/aws/r/ecs_service.html#placement_constraints-1) docs"
+  default     = []
+}
+
+variable "service_discovery_name" {
+  type        = string
+  description = "The name of the service."
+}
+
+variable "service_discovery_namespace_id" {
+  type        = string
+  description = "The ID of the namespace that you want to use to create the service."
+}
+
+variable "redis_maxmemory" {
+  type        = number
+  description = "Maxmemory is a Redis configuration that allows you to set the memory limit at which your eviction policy takes effect."
+  default     = 25
+}
