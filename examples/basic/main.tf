3f563b1274ab75b0373bcda8e305bc7c56844d13
Marco Frömbgen <23717573+mfroembgen@users.noreply.github.com>
2023-04-28 10:47:33 +0200

    feat: create_module (#2)

---
+++ examples/basic/main.tf
@@ -0,0 +1,8 @@
+module "redis" {
+  source = "../.."
+
+  ecs_cluster_arn                = "your-ecs-cluster"
+  service_discovery_name         = "redis"
+  service_discovery_namespace_id = "ns-0000000000000000"
+  vpc_id                         = "vpc-00000000000000000"
+}
